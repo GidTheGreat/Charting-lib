8644d145ab12a5f20f552d3069311ac88c5dd938
Gideon Wamalwa <63095691+GidTheGreat@users.noreply.github.com>
2026-06-04 19:57:00 +0300

    PEP 8 compliant

--- candles_utils.py
+++ candles_utils.py
@@ -1,109 +1,110 @@
+"""This module takes tick data and makes candle data.
+
+COMMENTS FOR ME
+Note this algo works best on tf_secs 60secs....have not tested it on other tfs
+....if to add more tf functionality....note last_complete_index
+
+ASSUMPTION: NO TWO ENTRIES CAN BE EQUIVALENT BECAUSE OF EPOCH
+"""
+
 import asyncio
-import pandas as pd
-from datetime import datetime
-import time
+#import time
 from collections import defaultdict
+from datetime import datetime
 
-"""Note this algo works best on tf_secs 60secs....have not tested it on other tfs....if to add more tf functionality....note last_complete_index"""
-
-
-"""ASSUMPTION: NO TWO ENTRIES CAN BE EQUIVALENT BECAUSE OF EPOCH"""
-
-dt = datetime.fromtimestamp
+import pandas as pd
 
 
-async def handle_candles(symbol,market_data,dsp=None): 
+async def handle_candles(symbol,market_data):
+    """Tick data is made into candle level data here
+    """
     i = 0
-    candles = []
     timeframes = {"1min":60}
     forming_candle = {}
-    last_complete_index = 0 # to keep track of intervals where close price is recorded and thus remove them from next interval processing
-    current_bin = None #to ensure open and close prices are from same interval
-       
-    while True: # make generator eternal
-        cpu_start = time.perf_counter()
+    #track intervals where close is recorded and thus remove them from next interval processing
+    last_complete_index = 0
+    #to ensure open and close prices are from same interval
+    current_bin = None
+    #make generator eternal
+    while True:
+        #cpu_start = time.perf_counter()
         tick_data = market_data[symbol]["tick_data"]
         print(datetime.now())
-        
-        while i < len(tick_data) and len(tick_data) >2: # ensure cursor is less than len of tick data before processing
+        while i < len(tick_data) and len(tick_data) >2:
+          #ensure cursor is less than len of tick data before processing
             i = len(tick_data)
-            current_slice = tick_data[last_complete_index:] # latest unprocessed slice
-            
+            #latest unprocessed slice
+            current_slice = tick_data[last_complete_index:]
             for tf, tf_secs in timeframes.items():
                 for idx,entry in enumerate(current_slice):
                     if tf_secs == 60:
                         forming_candle["count"] = datetime.fromtimestamp(entry["epoch"]).second
-                    if idx == 0: # to provent weird case of idx being -1
+                    #to provent weird case of idx being -1
+                    if idx == 0:
                         continue
-                    
-                    #print("index:",idx,"entry:",entry["quote"],datetime.fromtimestamp(entry["epoch"]))
-                    
-                    if entry["epoch"] // tf_secs != current_slice[idx-1]["epoch"] // tf_secs:
-                        assert entry != current_slice[idx-1], "Two entries in tickdata are equal ,REASSESS"
+                    if ((entry["epoch"] // tf_secs) !=
+                    (current_slice[idx-1]["epoch"] // tf_secs)):
+                        assert (
+                        entry != current_slice[idx-1]
+                        ), "Two entries in tickdata are equal ,REASSESS"
                         #print("entry:",datetime.fromtimestamp(entry["epoch"]))
                         forming_candle["open"] = entry["quote"]
                         forming_candle["epoch"] = entry["epoch"]
-                        current_bin = entry["epoch"] // tf_secs # to record bin of open price for later comparison
-                        
+                        #to record bin of open price for later comparison
+                        current_bin = entry["epoch"] // tf_secs
                         #get all the values from start price to end of bin and find max and min
-                        forming_candle["high"] = max([tick["quote"] for tick in current_slice[idx:] if tick["epoch"] // tf_secs == current_bin])
-                        forming_candle["low"] = min([tick["quote"] for tick in current_slice[idx:] if tick["epoch"] // tf_secs == current_bin])                        
-                        
-                        
-                        
-                    elif ((idx+1) < (len(current_slice)-1)) and entry["epoch"] // tf_secs != current_slice[idx+1]["epoch"] // tf_secs:
-                        assert entry != current_slice[idx+1], "Two entries in tickdata are equal ,REASSESS"
-                        if entry["epoch"] // tf_secs != current_bin: # to ensure close price belongs to same bin as open price
+                        forming_candle["high"] = max(
+                        (tick["quote"] for tick in current_slice[idx:]
+                        if tick["epoch"] // tf_secs == current_bin)
+                        )
+                        forming_candle["low"] = min(
+                        (tick["quote"] for tick in current_slice[idx:]
+                        if tick["epoch"] // tf_secs == current_bin)
+                        ) 
+                    elif (
+                      ((idx+1) < (len(current_slice)-1)) and (entry["epoch"] // tf_secs) !=
+                    (current_slice[idx+1]["epoch"] // tf_secs)
+                    ):
+                        assert (
+                        entry != current_slice[idx+1]
+                        ), "Two entries in tickdata are equal ,REASSESS"
+                        #ensure close price belongs to same bin as open price
+                        if entry["epoch"] // tf_secs != current_bin:
                             #print("INCOMPLETE CANDLE SKIPPING")
-                            continue
-                        #print("===================")
-                        #print("entry at close:",datetime.fromtimestamp(entry["epoch"]), "entry at open:",datetime.fromtimestamp(forming_candle["epoch"]))
-                        #print("==================")
-                        
+                            continue                    
                         forming_candle["close"] = entry["quote"]
                         last_complete_index = tick_data.index(entry)
+                        print(forming_candle)
                         del forming_candle["count"]
                         market_data[symbol][f"candles_{tf}"].append(forming_candle)
                         #dsp.flux(forming_candle)
                         #print(market_data[symbol][f"candles_{tf}"])
                         forming_candle ={}
-                        current_bin = None                                    
-         
-            
-            
-            #print(candles)
-            yield i,forming_candle, candles
-        cpu_end = time.perf_counter()
-        if dsp:
-            pass
-            #dsp.flux("cpu delay",f"{cpu_end -cpu_start} seconds")
-        else:
-            pass
-            #print(f"cpu delay:{cpu_end -cpu_start} seconds")
+                        current_bin = None               
+            yield i,forming_candle
         await asyncio.sleep(0.1)
 
-
+  
 async def run(symbol,market_data):
-    async for i,forming_candle,candles in handle_candles(symbol,market_data):
-        print(forming_candle["count"],"s")  
-              
-            
+    """Handle generator of candles
+    """
+    async for i,forming_candle in handle_candles(symbol,market_data):
+        print(forming_candle["count"],"s",i)
+
+  
 async def main():
-    #global tick_data
-    markets=[]
+    """Entry point
+    """
     market_data = defaultdict(lambda:defaultdict(list))
     asyncio.create_task(run("R",market_data))
     df = pd.read_csv("test.csv",index_col=0)
     lst = df.to_dict(orient="records")
-    
-    
-    
     lst = lst[0:75]
     for item in lst:
         market_data["R"]["tick_data"].append(item)
         await asyncio.sleep(0.1)
-    
-    
-        
+
+   
 if __name__ == "__main__":
     asyncio.run(main())
+    
